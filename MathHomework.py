0f46fb29abfa50b60e1b2c59e7eb3fbc77120e9b
FelixDavidson <gdolan9561@gmail.com>
2015-12-30 17:19:18 -0500

    This code taught someone how to use optional arguments. Math Teacher hate him. Learn how.
    I learned how to use optional arguments and I finished a few other
    problems.

--- MathHomework.py
+++ MathHomework.py
@@ -7,7 +7,7 @@ def subraction(x, y):
     sub = x - y
     print sub
 
-def mutiplication(x, y, z):
+def mutiplication(x, y, z = 1):
     mult = x * y * z
     print mult
 
@@ -22,7 +22,6 @@ def iamsolazy(x, y, z):
     lazy = x - y + z
     print lazy
 
-
 def average(x, y, z, a):
     # aka the most retarded problem. My dumbass could realizes that I should floating points when using averages. Fixed it by getting rid of the problem
     oldav = (x + y + z) / 3
@@ -33,7 +32,25 @@ def average(x, y, z, a):
     newav = (x + y + z + neednum) / 4
     print "%d + %d + %d + %d / 4 = %d" % (x, y, z, neednum, newav)
 
+def more(a, b, c, d):
+    answer = a / b - c * d
+    print answer
+
+def whysomany(a, b, c, d, e):
+    answer = a - b ** c * d + e
+    print answer
+
+def percents(x, y, z):
+    percent = x * z
+    answer = percent / y
+    print answer
+
 mutiplication(-5, 3, 2)
 problem(-6, -3, 2)
 iamsolazy(82, 15, 35)
 average(150, 210, 170, 180)
+more(8, 4, 3, 2)
+whysomany(12, 2, 2, 3, 5)
+percents(18, 72, 100)
+percents(12, 60, 100)
+mutiplication(0.3, 5000000)
