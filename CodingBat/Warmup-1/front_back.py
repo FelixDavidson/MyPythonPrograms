8e3e417e7d55325360373d66e1cf4985f8a39a5f
FelixDavidson <gdolan9561@gmail.com>
2016-03-05 16:15:50 -0500

    Day 2 of Challenge
    Did more challenges.

--- CodingBat/Warmup-1/front_back.py
+++ CodingBat/Warmup-1/front_back.py
@@ -1,3 +1,31 @@
 def front_back(str):
     splitted = list(str)
-    
+    if len(str) <= 1:
+        return str
+
+    container = splitted.pop(0), splitted.pop(-1)
+    splitted.insert(0, container[-1])
+    splitted.append(container[0])
+    return ''.join(splitted)
+
+"""
+Overall pretty good for me. The solution was a bit smaller.
+
+def front_back(str):
+  if len(str) <= 1:
+    return str
+
+  mid = str[1:len(str)-1]  # can be written as str[1:-1]
+
+  # last + mid + first
+  return str[len(str)-1] + mid + str[0]
+
+Redid mine to get it a bit smaller
+
+def front_back(str):
+    if len(str) <= 1:
+        return str
+
+    center = str[1:-1]
+    return str[-1] + mid + str[0]
+"""
---
+++ CodingBat/Warmup-2/string_bits.py
@@ -0,0 +1,5 @@
+def string_bits(str):
+    return str[::2]
+string_bits('Hello')
+string_bits('Hi')
+string_bits('Heeololeo')
