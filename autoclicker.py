493512385259263db7037f77c2bb265d81b55ec5
FelixDavidson <gdolan9561@gmail.com>
2016-03-09 10:30:05 -0500

    More Exercises and autocliker updated

--- autoclicker.py
+++ autoclicker.py
@@ -8,25 +8,22 @@ def click(x, y, z):
         win32api.SetCursorPos((x,y))
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-        time.sleep(.05)
+        time.sleep(.03)
 
 def clickany(z):
     for i in range(z):
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-        time.sleep(.05)
+        time.sleep(.03)
 
 def choose():
     print '1. Click at x 1200 y 400\n2. Click anywhere you want'
     choice = raw_input('> ')
+    print 'How many times?'
+    amount = int(raw_input('> '))
     if choice == '1':
-        print 'How many times?'
-        amount = raw_input('> ')
         click(1200, 400, amount)
-
     elif choice == '2':
-        print 'How many times?'
-        amount = raw_input('> ')
         clickany(amount)
     else:
         print "Feature not yet implimented"
