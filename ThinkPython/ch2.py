93d89f9a04862c6c12fac5ab8732d0ba2383b0fa
FelixDavidson <gdolan9561@gmail.com>
2016-03-18 23:43:49 -0400

    ThinkPython

---
+++ ThinkPython/ch2.py
@@ -0,0 +1,13 @@
+# Ex 2.2
+width = 17
+height = 12.0
+delimiter = '.'
+width/2 # 8
+width/2.0 # 8.5
+height/3 # 4.0
+1 + 2 * 5 # 11
+delimiter * 5 # .....
+
+# Ex 2.3
+r = 5 # radius for a sphere
+4.0 / 3.0 * 3.14 * r ** 3 # 532.3
