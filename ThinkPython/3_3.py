dedd42d40ba6c5c03916c5706d01eac2f242c6f3
FelixDavidson <gdolan9561@gmail.com>
2016-04-13 17:23:32 -0400

    Chapter 3 exercises
    I'm almost done with the exercises. I've put all the exercises into one file.

--- ThinkPython/3_3.py
+++
@@ -1,5 +0,0 @@
-def right_justify(s):
-    x = 70 - len(s)
-    print x * ' ' + s
-
-right_justify('allen')
---
+++ ThinkPython/ch3.py
@@ -0,0 +1,46 @@
+# 3_3
+def right_justify(s):
+    x = 70 - len(s)
+    print x * ' ' + s
+
+right_justify('allen')
+
+# 3_4
+def do_twice(f, value):
+    f(value)
+    f(value)
+
+def print_spam(value):
+    print 'spam' * value
+
+def print_twice(string):
+    print string * 2
+
+def do_four(func, value):
+    func(value)
+    func(value)
+
+do_twice(print_spam, 5)
+do_four(print_twice, 'foo')
+
+# 3_5
+def piped():
+    print '|',' ' * 4, '|', ' ' * 4, '|'
+
+def plusminus():
+    print '+', '-' * 4, '+', '-' * 4, '+'
+
+def grid():
+    plusminus()
+    piped()
+    piped()
+    piped()
+    piped()
+    plusminus()
+    piped()
+    piped()
+    piped()
+    piped()
+    plusminus()
+
+grid()
