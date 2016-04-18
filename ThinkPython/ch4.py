19d704cadf499dc1364bbf2cf5f25d7522ca8ac9
Gregory Dolan <gdolan9561@gmail.com>
2016-04-17 21:40:16 -0400

    Completed part of chapter 4 exercises
    Made shapes with turtles

--- ThinkPython/ch4.py
+++ ThinkPython/ch4.py
@@ -3,10 +3,20 @@ from swampy.TurtleWorld import *
 world = TurtleWorld()
 bob = Turtle()
 
-def square(t):
+def square(t, length):
     t = Turtle()
     for i in range(4):
-        fd(t, 100)
+        fd(t, length)
         lt(t)
 
-square(bob)
+square(bob, 50)
+
+def polygon(t, length, n):
+    t = Turtle()
+    for i in range(5):
+        fd(t, 100)
+        lt(t, 360/n)
+
+polygon(bob, 50, 5)
+
+def circle(
