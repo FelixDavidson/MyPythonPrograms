2e9a84f1d735ab5930359b9739cd69e6b8fc16a4
Gregory Dolan <gdolan9561@gmail.com>
2016-04-25 10:41:18 -0400

    Completed all the 4.3 exercises except the last

--- ThinkPython/4_3.py
+++ ThinkPython/4_3.py
@@ -1,7 +1,9 @@
+import math
 from swampy.TurtleWorld import *
 
 world = TurtleWorld()
 bob = Turtle()
+bob.delay = 0.01
 
 def square(t, length):
     for i in range(4):
@@ -19,6 +21,16 @@ def polygon(t,  n, length):
 polygon(bob, 5, 50)
 
 def circle(t, r):
-    
+    circumference = 2 * math.pi * r
+    n = int(circumference / 10)
+    print n
+    polygon(bob, n, 20)
 
-circle(bob)
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 10)
+    step_length = arc_length / 2
+    step_angle = float(angle) / 2
+    polygon
+circle(bob, 50)
+wait_for_user()
