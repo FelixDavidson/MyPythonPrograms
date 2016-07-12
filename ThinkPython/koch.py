0936d8f3bc20df058a9bf92cf72745032d973360
FelixDavidson <gdolan9561@gmail.com>
2016-07-12 16:37:09 -0400

    Generalized and refactored koch function

--- ThinkPython/koch.py
+++ ThinkPython/koch.py
@@ -1,23 +1,26 @@
 from swampy.TurtleWorld import *
 
 def koch(t, length):
-    fd(t, length/3)
+    if length < 3:
+        fd(t, length)
+        return
+    n = length / 3.0
+    koch(t, n)
     lt(t, 60)
-    fd(t, length/3)
+    koch(t, n)
     rt(t, 120)
-    fd(t, length/3)
+    koch(t, n)
     lt(t, 60)
-    fd(t, length/3)
-    rt(t, 120)
+    koch(t, n)
 
 def snowflake(t, length):
-    koch(t, length)
-    koch(t, length)
-    koch(t, length)
+    for i in range(3):
+        koch(t, length)
+        rt(t, 120)
 
 world = TurtleWorld()
 bob = Turtle()
-bob.delay = 0.01
+bob.delay = 0
 
-snowflake(bob, 60)
+snowflake(bob, 120)
 wait_for_user()
